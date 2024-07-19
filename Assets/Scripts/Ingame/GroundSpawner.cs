fee28a488dca8befdab6af1555fa885b727c58a3
Woojin Kwak <kwooj2788@kookmin.ac.kr>
2024-07-19 14:58:24 +0900

    [FEAT] 맵 무작위 생성 기능 구현

--- Assets/Scripts/Ingame/GroundSpawner.cs
+++ Assets/Scripts/Ingame/GroundSpawner.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GroundSpawner : MonoBehaviour
 {
     public GameObject prefab;
     public Transform groundCenter;
-    public int gridSize = 10;
-    public float spacing = 1.1f;
-    public int maxNumber = 5;
+    public int gridSize;
+    public float spacing;
+    public int maxNumber;
+    public int holeRatio;
 
     void Start( )
     {
@@ -16,19 +18,40 @@ public class GroundSpawner : MonoBehaviour
     public void SpawnGround( )
     {
         Vector3 gridCenter = new Vector3( ( gridSize - 1 ) * spacing / 2.0f, 0, ( gridSize - 1 ) * spacing / 2.0f );
-        
-        for( int x = 0; x < gridSize; x++ )
+        List<Vector3> positions = new List<Vector3>();
+
+        for (int x = 0; x < gridSize; x++)
         {
-            for( int z = 0; z < gridSize; z++ )
+            for (int z = 0; z < gridSize; z++)
             {
-                if(Random.value <= 0.05f)
-                    continue;
-                
-                Vector3 spawnPosition = new Vector3( x * spacing, 0, z * spacing ) - gridCenter;
-                GameObject instance = Instantiate( prefab, spawnPosition, Quaternion.identity );
-                instance.GetComponent<NumberCube>(  ).SetNumber( maxNumber );
-                instance.transform.SetParent( groundCenter );
+                Vector3 spawnPosition = new Vector3(x * spacing, 0, z * spacing) - gridCenter;
+                positions.Add(spawnPosition);
             }
+        }
+        
+        Shuffle(positions);
+
+        float holeProbability = holeRatio / 100f;
+        foreach (Vector3 position in positions)
+        {
+            if(Random.value <= holeProbability)
+                if(position != Vector3.zero)
+                    continue;
+            
+            GameObject instance = Instantiate( prefab, position, Quaternion.identity );
+            instance.GetComponent<NumberCube>(  ).SetNumber( maxNumber );
+            instance.transform.SetParent( groundCenter );
+        }
+    }
+    
+    private void Shuffle( List<Vector3> list )
+    {
+        for ( int i = 0; i < list.Count; i++ )
+        {
+            Vector3 temp = list[i];
+            int randomIndex = Random.Range( i, list.Count );
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
         }
     }
 }
